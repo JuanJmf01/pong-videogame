52ae3fc5fe114c65c360d7a2850eec8ca51ef3bc
piperuao26 <110473121+piperuao26@users.noreply.github.com>
2023-09-26 20:48:09 +0000

    Creacion jugadores

--- Pong/pong.py
+++ Pong/pong.py
@@ -5,5 +5,28 @@ import turtle
 wn = turtle.Screen()
 wn.title ("Pong")
 wn.bgcolor ("blue")
+wn.setup(width = 800, height=600 )
+wn.tracer(0)
 
 
+#jugador 1
+jugador1 = turtle.Turtle()
+jugador1.speed(0)
+jugador1.shape("square")
+jugador1.color("green")
+jugador1.penup()
+jugador1.goto(-400,0)
+jugador1.shapesize(stretch_wid=5, stretch_len=1)
+
+#jugador2
+jugador2 = turtle.Turtle()
+jugador2.speed(0)
+jugador2.shape("square")
+jugador2.color("green")
+jugador2.penup()
+jugador2.goto(-400,0)
+jugador2.shapesize(stretch_wid=5, stretch_len=1)
+
+
+while True: 
+    wn.update()
