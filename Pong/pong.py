45787d74b79d93bd72d33f9278e4623388b7605c
piperuao26 <110473121+piperuao26@users.noreply.github.com>
2023-09-26 20:53:25 +0000

    Creacion bola :l

--- Pong/pong.py
+++ Pong/pong.py
@@ -4,7 +4,7 @@ import turtle
 #ventana 
 wn = turtle.Screen()
 wn.title ("Pong")
-wn.bgcolor ("blue")
+wn.bgcolor ("black")
 wn.setup(width = 800, height=600 )
 wn.tracer(0)
 
@@ -13,7 +13,7 @@ wn.tracer(0)
 jugador1 = turtle.Turtle()
 jugador1.speed(0)
 jugador1.shape("square")
-jugador1.color("green")
+jugador1.color("white")
 jugador1.penup()
 jugador1.goto(-400,0)
 jugador1.shapesize(stretch_wid=5, stretch_len=1)
@@ -22,10 +22,21 @@ jugador1.shapesize(stretch_wid=5, stretch_len=1)
 jugador2 = turtle.Turtle()
 jugador2.speed(0)
 jugador2.shape("square")
-jugador2.color("green")
+jugador2.color("white")
 jugador2.penup()
-jugador2.goto(-400,0)
+jugador2.goto(400,0)
 jugador2.shapesize(stretch_wid=5, stretch_len=1)
+
+#bola
+bola = turtle.Turtle()
+bola.speed(0)
+bola.shape("circle")
+bola.color("white")
+bola.penup()
+bola.goto(0,0)
+
+
+#Linea Division
 
 
 while True: 
