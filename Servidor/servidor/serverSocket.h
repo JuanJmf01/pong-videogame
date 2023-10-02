2de4bf34ccf5e10cc976e99b2f1e72228a7bc409
juanjmf01 <juaneltrozo13@gmail.com>
2023-10-02 12:27:23 -0500

    Organizacion estructural del codigo

---
+++ Servidor/servidor/serverSocket.h
@@ -0,0 +1,11 @@
+//servidor/serverSocket.h
+
+
+#ifndef SERVER_SOCKET_H
+#define SERVER_SOCKET_H
+
+#include "../variablesCompartidas.h" // Incluye el archivo de cabecera que define tus estructuras
+
+void *defineSocket(void *juegoDatos);
+
+#endif // MANEJAR_PELOTA_H
---
+++ Servidor/variablesCompartidas.c
@@ -0,0 +1,8 @@
+#include "variablesCompartidas.h"
+
+int cliente;
+int server_socket;
+int client_socket;
+
+struct DatosDeJuego datosDeJuego[1];
+struct ClientInfo clients[2];
